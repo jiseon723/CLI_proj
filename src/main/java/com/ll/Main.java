8c9260211fc5e6c5031f12bf44ec18695994dbd9
jiseon723 <dighxxx12@gmail.com>
2025-07-20 13:50:30 +0900

    remove

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -37,6 +37,20 @@ public class Main {
                         Article article = articleList.get(i);
                         System.out.printf(" %d / %s / %s ", article.getId(), article.getSubject(), article.getContent());
                 }
+            } else if (command.startsWith("삭제")){
+                String[] commandList = command.split("\\?", 2);
+                String[] paramsStr = commandList[1].split("=", 2);
+
+                String value = paramsStr[1];
+                int idx = Integer.parseInt(value);
+
+                for (int i = 0; i < articleList.size(); i++) {
+                    if (articleList.get(i).getId() == idx) {
+                        articleList.remove(i);
+                    }
+                }
+
+                System.out.printf("%d번 게시물이 삭제되었습니다.\n", idx);
             }
         }
         sc.close();
